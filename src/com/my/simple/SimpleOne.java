2177cbde0b09faba66de04c818514a18a00a7de7
Max Tymchii <Samback@ukr.net>
2011-08-25 20:24:37 +0300

    Changes
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/simple/SimpleOne.java
+++ src/com/my/simple/SimpleOne.java
@@ -2,8 +2,11 @@ package com.my.simple;
 
 public class SimpleOne {
 	public static void main(String[] args) {
+		
 		for (int i = 0; i < 10; i++)
 			System.out.println("Hello world !"+ i+ "!!");
+		
+		System.out.println("My own");
 	}
 
 }
