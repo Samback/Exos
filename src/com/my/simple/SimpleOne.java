7622441aa23b54172140702bb90d4c9639b5831e
Max Tymchii <Samback@ukr.net>
2011-08-25 20:02:25 +0300

    Create a new one looop
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/simple/SimpleOne.java
+++ src/com/my/simple/SimpleOne.java
@@ -1,5 +1,9 @@
 package com.my.simple;
 
 public class SimpleOne {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++)
+			System.out.println("Hello world !"+ i+ "!!");
+	}
 
 }
