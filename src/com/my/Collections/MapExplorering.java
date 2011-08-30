8f58bfd290cdf10d90de4bcec6a1805eaefb8117
Max Tymchii <Samback@ukr.net>
2011-08-30 11:55:41 +0300

    HashMap, HashSet, LinkedHashMap
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Collections/MapExplorering.java
+++ src/com/my/Collections/MapExplorering.java
@@ -1,5 +1,11 @@
 package com.my.Collections;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MapExplorering {
 
 	/**
@@ -7,7 +13,19 @@ public class MapExplorering {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("abcde", 5);
+		hm.put("abc", 3);
+		hm.put("abcd", 4);
+		hm.put("aaaaaa", 1);
 		
+		System.out.println(hm);
+		
+		TreeMap<String, Integer> tm = new TreeMap<String, Integer>(hm);
+		System.out.println(tm);
+		
+		LinkedHashMap<String, Integer> lhm = new LinkedHashMap<String, Integer>(tm);
+		System.out.println(lhm);
 
 	}
 
