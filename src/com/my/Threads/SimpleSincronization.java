dc5537b520ee06bb49b6b3bdda6850a92a34e240
Max Tymchii <Samback@ukr.net>
2011-08-30 23:33:07 +0300

    Last Commit at this class
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Threads/SimpleSincronization.java
+++ src/com/my/Threads/SimpleSincronization.java
@@ -3,9 +3,14 @@ package com.my.Threads;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Ones {
 	private static int im = 0;
+	private static int inn = 0;
+	
+	private Lock lockk = new ReentrantLock();
 	synchronized int next()
 	{
 		im ++;
@@ -13,6 +18,22 @@ class Ones {
 		im ++;
 		Thread.currentThread().yield();	
 		return im;
+	}	
+	int nextInt() throws InterruptedException
+	{
+		
+		lockk.lock();
+		try{
+		inn ++;
+		Thread.currentThread().sleep(1000);
+		inn ++;
+	
+		return inn;
+		}finally{
+			lockk.unlock();
+		}
+		
+		
 	}
 }
 class MyOwnRunnable implements Runnable{
@@ -22,7 +43,7 @@ private static Ones one = new Ones();
 		for (int i = 0; i < 10; i++)
 		{			
 			try {
-				System.out.println(Thread.currentThread().getId()+ "->"+one.next());
+				System.out.println(Thread.currentThread().getId()+ "->"+one.nextInt());
 				TimeUnit.MILLISECONDS.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -38,8 +59,19 @@ public class SimpleSincronization {
 	public static void main(String[] args) {
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		for (int i = 0; i < 10; i++)
+		{
 			executor.execute(new MyOwnRunnable());
+			System.out.println("=========");
+		}
 		executor.shutdown();
+		
+		ExecutorService exec = Executors.newCachedThreadPool();
+		for (int i = 0; i < 10; i++)
+			{
+			exec.execute(new MyOwnRunnable());
+			System.out.println("++++++++++++++");
+			}
+		exec.shutdown();
 	}
 	
 
