ca02844b55ffe9b367bb1fa05a39bcd63060abe6
Max Tymchii <Samback@ukr.net>
2011-08-30 10:48:43 +0300

    New branch for set explorering
    Signed-off-by: Max Tymchii <Samback@ukr.net>

---
+++ src/com/my/Collections/SetExplorering.java
@@ -0,0 +1,13 @@
+package com.my.Collections;
+
+public class SetExplorering {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
