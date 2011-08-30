cc621d059b6d0fa22298d65ffbf3765d47b885d9
Max Tymchii <Samback@ukr.net>
2011-08-30 22:07:16 +0300

    Create simple example of synchronization
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Threads/SimpleSincronization.java
+++ src/com/my/Threads/SimpleSincronization.java
@@ -1,6 +1,46 @@
 package com.my.Threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Ones {
+	private static int im = 0;
+	synchronized int next()
+	{
+		im ++;
+		Thread.currentThread().yield();
+		im ++;
+		Thread.currentThread().yield();	
+		return im;
+	}
+}
+class MyOwnRunnable implements Runnable{
+private static Ones one = new Ones();
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++)
+		{			
+			try {
+				System.out.println(Thread.currentThread().getId()+ "->"+one.next());
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
 
 public class SimpleSincronization {
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 10; i++)
+			executor.execute(new MyOwnRunnable());
+		executor.shutdown();
+	}
+	
 
 }
