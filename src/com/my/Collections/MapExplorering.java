a2702b204b6d52461b84a904362a070038448c06
Max Tymchii <Samback@ukr.net>
2011-08-30 11:09:40 +0300

    Starting exploring maps
    Signed-off-by: Max Tymchii <Samback@ukr.net>

---
+++ src/com/my/Collections/MapExplorering.java
@@ -0,0 +1,14 @@
+package com.my.Collections;
+
+public class MapExplorering {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
