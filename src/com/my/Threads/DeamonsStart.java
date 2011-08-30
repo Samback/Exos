90790afafe6eabbadbb4f1f1d9047bc07ae98bff
Max Tymchii <Samback@ukr.net>
2011-08-30 16:41:19 +0300

    The worked example of daemons factory
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Threads/DeamonsStart.java
+++ src/com/my/Threads/DeamonsStart.java
@@ -1,5 +1,30 @@
 package com.my.Threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+
+class DeamonsFactorys implements Runnable{
+
+	@Override
+	public void run() {
+		while (true)
+		{			
+			try {
+				System.out.println(Thread.currentThread().getId()+ " ");
+				TimeUnit.SECONDS.sleep(1);
+			
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+}
 public class DeamonsStart {
 
 	/**
@@ -7,6 +32,17 @@ public class DeamonsStart {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ExecutorService exec = Executors.newCachedThreadPool(new DaemonThreadFactory());
+		for(int i = 0; i < 6; i++)
+			exec.execute(new DeamonsFactorys());
+		System.out.println("Now deamons are free!!!!1");	
+		try {
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("Thats all");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
