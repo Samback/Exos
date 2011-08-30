bfb41174fe4c79e488823957c8b21439eb83a641
Max Tymchii <Samback@ukr.net>
2011-08-30 12:04:30 +0300

    New Branch and new class Signed-off-by: Max Tymchii <Samback@ukr.net>

---
+++ src/com/my/Collections/CollectionAndIterator.java
@@ -0,0 +1,13 @@
+package com.my.Collections;
+
+public class CollectionAndIterator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
