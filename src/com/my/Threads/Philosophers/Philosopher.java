3c6eaee4d591dabe64725b74502bd9d234d6e167
Max Tymchii <Samback@ukr.net>
2011-08-31 10:52:03 +0300

    Class Philosopher created
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Threads/Philosophers/Philosopher.java
+++ src/com/my/Threads/Philosophers/Philosopher.java
@@ -1,11 +1,48 @@
 package com.my.Threads.Philosophers;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class Philosopher implements Runnable {
+
+	private Chopstik left;
+	private Chopstik right;
+
+	private final int id;
+	private final int ponderFactor;
+	private Random rand = new Random(47);
+
+	private void pause() throws InterruptedException {
+		if (ponderFactor == 0)
+			return;
+		TimeUnit.MILLISECONDS.sleep(rand.nextInt(ponderFactor * 250));
+	}
+
+	public Philosopher(Chopstik left, Chopstik right, int ident, int ponder) {
+		this.left = left;
+		this.right = right;
+		id = ident;
+		ponderFactor = ponder;
+	}
+	@Override
+	public String toString () { return "Philosopher " + id;}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try{
+			while(!Thread.interrupted()){
+				System.out.println(this + " thinking");
+				//takes right 
+				System.out.println(this + " take right");
+				right.take();
+				
+				//takes left
+				System.out.println(this + " take left");
+				left.take();
+			}
+		}catch (InterruptedException e) {
+			System.out.println("exit interupted" + this);
+		}
 
 	}
-
 }
