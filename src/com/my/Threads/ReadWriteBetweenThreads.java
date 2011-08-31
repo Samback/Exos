b54ebd42109b7112c4148af294420e0e418bf2ef
Max Tymchii <Samback@ukr.net>
2011-08-31 09:26:09 +0300

    Sender Created
    Signed-off-by: Max Tymchii <Samback@ukr.net>

--- src/com/my/Threads/ReadWriteBetweenThreads.java
+++ src/com/my/Threads/ReadWriteBetweenThreads.java
@@ -1,12 +1,44 @@
 package com.my.Threads;
 
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.util.concurrent.TimeUnit;
+
+class Sender implements Runnable {
+
+	private PipedWriter out = new PipedWriter();
+
+	@Override
+	public void run() {
+		try {
+			while (true)
+				for (char c = 'A'; c < 'z'; c++) {
+					out.write(c);
+					TimeUnit.MILLISECONDS.sleep(100);
+				}
+		} catch (IOException e) {
+			System.out.print(e);
+		} catch (InterruptedException e) {
+			System.out.print(e);
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
+
 public class ReadWriteBetweenThreads {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
